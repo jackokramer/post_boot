6183fcfb8068ead68cda2a4e872e60713fc524e4
jackokramer <jackokramer@gmail.com>
2020-03-31 21:17:28 -0700

    second

--- es6/beatles.js
+++ es6/beatles.js
@@ -1,7 +1,7 @@
 var beatles = ['Paul', 'George', 'Ringo', 'Johnny'];
 function printNames(names){
     function actuallyPrintNames(){
-    for(var x = 0; x<names.length; x++){
+    for(var x = 0; x<names.length; x++) {
         var names = names[x];
 
         console.log(name + 'was found in x spot' + x);
@@ -15,3 +15,21 @@ printNames(beatles);
 var stones = ['Keith', 'Mick', 'Bill', ' Ronnie', 'Charly'];
 
 printNames(stones);
+
+// ECHMA 6 would have it written like this
+
+const zepplin = ['Jimmy', 'Robert', 'John Paul', 'Bonzo'];
+function printBand(band){
+    function actuallyPrintBands(){
+        //let index = 0;
+        //let name;
+        for(let index = 0; index<name.length; index++){
+            const name = name[index];
+            console.log(name + 'was found at index' + index);
+        }
+        console.log('name and index agetr loop is ' + name + ':' + index);
+    }
+    actuallyPrintBands();
+}
+
+

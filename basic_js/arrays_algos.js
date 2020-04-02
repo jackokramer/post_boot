35a6ce8a96afb6ca7404e3002f8bc5597f15393a
jackokramer <jackokramer@gmail.com>
2020-04-01 17:00:02 -0700

    fifs

---
+++ basic_js/arrays_algos.js
@@ -0,0 +1,20 @@
+// Print Values and Sum
+
+let testArr = [6,3,5,1,2,4];
+//let sum = 0;
+for(let x = 0; x<testArr.length; x++){
+    x += testArr[x];
+}
+
+console.log(testArr[1]);
+
+//Values and Positions
+// Multiply each value in the array by its position
+
+let valArr = [6,3,5,1,2,4];
+let summ = 0;
+for(var t = 0; valArr.lengt>t; t++){
+    valArr[t] * t;
+}
+
+console.log(valArr[2]);
---
+++ es6/arrays_algos.js
@@ -0,0 +1,7 @@
+let testArr = [6,3,5,1,2,4];
+let sum = 0;
+for(let x = 0; x<testArr.length; x++){
+    sum += testArr[x];
+}
+
+console.log(testArr[0]);

561d5bcada807a4f6728f4e720238db7041b8b38
jackokramer <jackokramer@gmail.com>
2020-04-02 16:11:50 -0700

    jarule

--- basic_js/basic_count.js
+++ basic_js/basic_count.js
@@ -1,4 +1,4 @@
-function countNum(startnum){
+function countNum(startnum){ // function expects a parameter in order to run
     for(var x = startnum; x>=0; x--){ // needs to decrement or its a stack overflow/inifite loop
         console.log(x);
     }

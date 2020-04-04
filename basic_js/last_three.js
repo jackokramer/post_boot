b3a9cdf6ce2c895cfc5951b19ca63978c61280cc
jackokramer <jackokramer@gmail.com>
2020-04-03 21:51:11 -0700

    fours

--- basic_js/last_three.js
+++ basic_js/last_three.js
@@ -3,8 +3,8 @@
 function imHungry(arr){
     //var sum = [];
     for(var x = 0; arr.length>x; x++){
-        if(arr[x] = 'food'){
-            console.log('yummy')
+        if(arr[x] === 'food'){ // needs a double or triple equals of food
+            console.log('yummy');
         } else {
             console.log('Im hungry')
         }
@@ -16,17 +16,18 @@ console.log(imHungry(['stuff', 'baking soda', 'food', 'zombies']));
 
 // Swap Toward the Center - Given an array, swap the first and last values, third and third-to-last values, etc.  Example: swapTowardCenter([true,42,"Ada",2,"pizza"]) turns the array into ["pizza", 42, "Ada", 2, true].  swapTowardCenter([1,2,3,4,5,6]) turns the array into [6,2,4,3,5,1].  No need to return the array this time.
 function swapTowardCenter(arr){
-    var array = [];
-    for(var y = 0; arr.length>y; y++){
+    //var array = []; // no need for this
+    for(var y = 0; arr.length/2>y; y+=2){ // trick here is to divide arr.length by 2 and increment by 2
         var temp = arr[0];
         arr[0] = arr[arr.length-1 -y];
         arr[arr.length-1- y] = temp;
     }
-    console.log(arr);
+    //console.log(arr);// and return arr
+    return arr;
 }
 
-console.log(swapTowardCenter([2,3,5,62,2,6,7]));
-console.log(swapTowardCenter(['pizza',3,5,62,2,'chicken nuggets',7]));
+//console.log(swapTowardCenter([2,3,5,62,2,6,7]));
+//console.log(swapTowardCenter(['pizza',3,5,62,2,'chicken nuggets',7]));
 
 //Scale the Array - Given an array arr and a number num, multiply all values in the array arr by the number num, and return the changed array arr.  For example, scaleArray([1,2,3], 3) should return [3,6,9].
 
@@ -34,10 +35,13 @@ console.log(swapTowardCenter(['pizza',3,5,62,2,'chicken nuggets',7]));
 
 function negativeMaker(arr){
     var newArr = [];
-    for(var p =0; arr.length>p; p++){
+    for(var p = 0; arr.length>p; p++){
         if(arr[p]>0){
-            arr[p] * -1;
+            arr[p]= arr[p]*-1;
         }
+        newArr.push(arr[p])
     }
     return newArr;
 }
+
+//console.log(negativeMaker([1,2,-5,-4]));

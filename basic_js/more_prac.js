69d1191bd2d33fc5453eb23fc62071bee483d05a
jackoramer <jackokramer@gmail.com>
2020-04-05 19:12:14 -0700

    zappa

--- basic_js/more_prac.js
+++ basic_js/more_prac.js
@@ -6,6 +6,7 @@ function outlookNeg(arr){
         if(arr[x]>0){
             newArr = arr[x] *1; 
         }
+        newArr.push(arr[i]);
     }
     return newArr;
 }
@@ -26,32 +27,39 @@ function biggieSize(arr){
 //Print Low, Return High - Create a function that takes in an array of numbers.  The function should print the lowest value in the array, and return the highest value in the array.
 
 function printLowReturnHigh(arr){
-        console.log(arr[0]);
-        return arr.length -1;
+    var low = arr[0];
+    var high = arr[0];
+    for(var c = 1; arr.length>c; c++){
+        if(arr[c]<low){
+            low = arr[c];
+        } else if(arr[c]>high){
+            high = arr[c];
+        }
+    }
+    console.log(low);
+    return high;
 }
 
 //Print One, Return Another - Build a function that takes in an array of numbers.  The function should print the second-to-last value in the array, and return the first odd value in the array.
-function printOneReturnAnother(arr){
+function printOneReturnAnother(arr){ // don't need a for loop
+    console.log(arr[arr.length-2]);
     for(var t = 0; arr.length>t; t++){
-        console.log(arr.length - t -1);
-    }
-    if(arr[t]%2 === 1){
-        return arr[t];
+        if(arr[t]%2 == 1){
+            return arr[t];
+        }
     }
 }
 
-//console.log(printOneReturnAnother([1,3,-6,24,30,17])); // Not right
+console.log(printOneReturnAnother([1,3,-6,24,30,17]));
 
 // Double Vision - Given an array (similar to saying 'takes in an array'), create a function that returns a new array where each value in the original array has been doubled.  Calling double([1,2,3]) should return [2,4,6] without changing the original array.
 
 function doubleVision(arr){
     let newt = [];
     for(var z = 0; arr.length>z; z++){
-        if(arr[z]){
-            newt = arr[z] * 2;
-        }
+            newt.push(arr[z] * 2);
     }
     return newt;
 }
 
-console.log(doubleVision([1,2,6,7,88]));
+//console.log(doubleVision([1,2,6,7,88]));

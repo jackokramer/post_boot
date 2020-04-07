7e5cebebe264950e11cfe187f37e7ce947bff29d
jackokramer <jackokramer@gmail.com>
2020-04-06 18:33:00 -0700

    alex

---
+++ basic_js/playobj.js
@@ -0,0 +1,21 @@
+let users = [
+    {
+        name: 'Jack',
+        age: 29
+    },
+    {
+        name: 'Andy',
+        age: 28
+    },
+    {
+        name: 'Nina',
+        age: 23
+    }
+
+];
+
+console.log([users[0].name,' - ', users[0].age]);
+
+console.log([users[1].name,' - ', users[1].age]);
+
+console.log([users[2].name, ' - ', users[2].age]);

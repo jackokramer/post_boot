cba91ae64fe9f23061c18bf669996ac9ae56a01b
jackokramer <jackokramer@gmail.com>
2020-04-07 18:18:17 -0700

    lgmeats

---
+++ basic_js/scale.js
@@ -0,0 +1,10 @@
+// Scale the Array - Given an array arr and a number num, multiply all values in the array arr by the number num, and return the changed array arr.  For example, scaleArray([1,2,3], 3) should return [3,6,9].
+
+function scaleArray(arr, num){
+    for(var x = 0; arr.length>x; x++){
+        arr[x] = arr[x] * num;
+    }
+    return arr;
+}
+
+console.log(scaleArray([1,2,5],6));

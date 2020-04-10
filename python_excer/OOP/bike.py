45ca095619ec6173f961620bc81fbd0917b04e1a
jackokramer <jackokramer@gmail.com>
2020-04-10 16:32:04 -0700

    bike

---
+++ python_excer/OOP/bike.py
@@ -0,0 +1,14 @@
+class Bike:
+    def __init__(self, price, max_speed, miles):
+        self.price = 0
+        self.max_speed = 0
+        self.miles = 0
+    def displayInfo(self):
+        print(f'New bike to purchase at ${self.price} and it can reach a max speed at {self.max_speed} mph and it has {self.miles} on it.')
+        return self
+    def riding(self):
+        self.miles +=10
+        print(f"You test rode at {self.miles}. How was it?")
+    def reverse(self):
+        if self.ride >= 10:
+            self.miles -=5:

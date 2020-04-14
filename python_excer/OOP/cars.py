f2c08f28c1ff93e45632d972168ee5abd5cf131f
jackokramer <jackokramer@gmail.com>
2020-04-14 15:18:23 -0700

    fors

--- python_excer/OOP/cars.py
+++ python_excer/OOP/cars.py
@@ -1,2 +1,8 @@
-class Cars():
-    def __init__(self, Price, Speed, Fuel, Mileage, Tax)
+class Cars:
+    def __init__(self, price, speed, fuel, mileage, tax):
+        self.price = 0
+        self.speed = 0
+        self.fuel = 0
+        self.mileage =0
+        self.tax = .12
+    
--- python_excer/OOP/rec_fibonacci.py
+++ python_excer/OOP/rec_fibonacci.py
@@ -2,3 +2,5 @@ def fibnoacci(n):
     if n<2: return n
     else:
         return (fibnoacci(n-1)+ fibnoacci(n-2))
+
+fibnoacci(6)

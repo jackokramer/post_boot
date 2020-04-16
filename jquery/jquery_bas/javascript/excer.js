e6b94f5d9813385dd01ec8901ad34394d7bddc46
jackokramer <jackokramer@gmail.com>
2020-04-15 21:33:50 -0700

    cars

---
+++ jquery/jquery_bas/javascript/excer.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('button').click(function(){
+        $('p').addClass('blue');
+        $('div').addClass('important');
+    });
+    //$('h1').hover(function(){
+    //    $('h2').text('go to the beach');
+    //}, function(){alert('now invoking the second function')});
+});
--- python_excer/OOP/cars.py
+++ python_excer/OOP/cars.py
@@ -1,8 +1,16 @@
 class Cars:
     def __init__(self, price, speed, fuel, mileage, tax):
-        self.price = 0
-        self.speed = 0
-        self.fuel = 0
-        self.mileage =0
-        self.tax = .12
-    
+        self.price = price
+        self.speed = speed
+        self.fuel = fuel
+        self.mileage = mileage
+        self.tax = tax
+    def description(self):
+        return '${} and has {}mph and is {} as well as about {}mpgs with a tax of {}'.format(self.price, self.speed, self.fuel, self.mileage, self.tax)
+
+car1 = Cars(2000, 35, 'full', 15, .12)
+car2 = Cars(2000, 5, 'Not Full', 105, .12)
+car3 = Cars(2000, 15, 'Kind of Full', 95, .12)
+car4 = Cars(2000, 25, 'Kind of Full', 95, .12)
+car5 = Cars(2000, 45, 'Empty', 25, .12)
+car6 = Cars(2000000, 35, 'Empty', 15, .15)

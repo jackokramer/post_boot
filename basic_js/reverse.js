e125846fead3785d53824c54e51babbec08b175a
jackokramer <jackokramer@gmail.com>
2020-04-19 20:35:41 -0700

    stugg

--- basic_js/reverse.js
+++ basic_js/reverse.js
@@ -1,15 +1,15 @@
 //Reverse Array - Given an array, write a function that reverses its values, in-place.  Example: reverse([3,1,6,4,2]) returns the same array, but now contains values reversed like so... [2,4,6,1,3].  Do this without creating an empty temporary array.  (Hint: you'll need to swap values).
 
 function reverseArr(arr){
-    for(let x = arr.length; arr.length>x/2; x++){
+    for(let x = 0; arr.length/2>x; x++){
         let temp = arr[x];
-        arr[x] = arr[arr-x-1];
-        arr[arr-x-1] = temp;
+        arr[x] = arr[arr.length-x-1];
+        arr[arr.length-x-1] = temp;
     }
     return arr;
 }
 
-//console.log(reverseArr([4,3,2,7,9,11]));
+console.log(reverseArr([4,3,2,7,17,9,11]));
 
 // Outlook: Negative - Given an array, create and return a new one containing all the values of the original array, but make them all negative (not simply multiplied by -1). Given [1,-3,5], return [-1,-3,-5].
 
@@ -23,4 +23,4 @@ function outlookNeg(arr){
     }
 }
 
-console.log(outlookNeg([2,4,5,2, -9, -7, 4]));
+//console.log(outlookNeg([2,4,5,2, -9, -7, 4]));

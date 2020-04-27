ac248a350f025c8eba909e4f79408fb614c413c9
jackokramer <jackokramer@gmail.com>
2020-04-27 15:36:44 -0700

    oops

---
+++ python_excer/OOP/animal.py
@@ -0,0 +1,32 @@
+class Animal:
+    def __init__(self, name, health):
+        self.name = name
+        self.health = health
+    def walk(self):
+        self.health -= 1
+        return self
+    def run(self):
+        self.health -= 5
+        return self
+    def display(self):
+        self.health
+        return self
+
+class Dog(Animal):
+    def __init__(self):
+        super().__init__()
+        self.health = 150
+    def pet(self):
+        self.health +=5
+        return self
+
+#Andy = Dog('Andy')
+
+class Dragon(Animal):
+    def __init__(self):
+        super().__init__()
+        self.health = 170
+    def fly(self):
+        self.health -= 10
+    def display(self):
+        print("I am a dragon")

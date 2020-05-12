e8bb52f686a77f100153da0a3d50d1544112d95f
jackokramer <jackokramer@gmail.com>
2020-05-11 21:13:01 -0700

    slavic

---
+++ es6/advance_js/linked_lists.js
@@ -0,0 +1,18 @@
+//NODE EXAMPLE DATA Structure
+class Node{
+    constructor(val){
+        this.value = val;
+        this.next = null;
+    }
+} //simple example of a data structure
+
+//EXAMPLE OF SINGLE LIST DATA STRUCTURES
+class SLlist{
+    constructor(){
+        this.head = null;
+    }
+    //all class methods go here
+    //var myList = newSLlist();
+}
+
+//write functions as class methods instead

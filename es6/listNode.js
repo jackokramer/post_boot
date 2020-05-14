97674206696a1f7227999bd61b1717770a9c4f53
jackokramer <jackokramer@gmail.com>
2020-05-13 21:06:38 -0700

    asw

--- es6/listNode.js
+++ es6/listNode.js
@@ -4,8 +4,26 @@ class listNode{
         this.next = null
     }
 }
-clas singleList{
+class singleList{
     constructor(){
         this.head = null;
+    }
+    addfront(value){
+        let node = newNode(value);
+        node.next=this.head;
+        this.head = node;
+        return this;
+    }
+    removeFront(){
+        if (this.head != null){
+            this.head = this.head.next
+        }
+        return this;
+    }
+    front(){
+        if(this.head == null){
+            return null
+        }
+        return this.head.value;
     }
 }

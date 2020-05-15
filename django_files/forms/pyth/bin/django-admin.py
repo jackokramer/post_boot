b58b54b059e07aab73a731eae2afbb2830c34a7e
jackokramer <jackokramer@gmail.com>
2020-05-15 11:22:29 -0700

    save

---
+++ django_files/forms/pyth/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/jackquentinkramer/Desktop/post_doj/django_files/forms/pyth/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()

d46f814979fc5b06a1d37502ef853041437b395a
jackokramer <jackokramer@gmail.com>
2020-05-15 11:44:12 -0700

    gei

--- django_files/forms/form_proj/form_app/views.py
+++ django_files/forms/form_proj/form_app/views.py
@@ -1,5 +1,11 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, HttpResponse
 
 # Create your views here.
 def index(request):
     return render(request,'index.html')
+
+def process(request):
+    if request.method != 'POST':
+        return redirect('/')
+    print(request.POST)
+    return HttpResponse('This is a post route')

1b234aeca316070d1f6a2a4261d2fb9ffcde303b
jackokramer <jackokramer@gmail.com>
2020-05-15 19:09:20 -0700

    saw

--- django_files/forms/form_proj/form_app/urls.py
+++ django_files/forms/form_proj/form_app/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('process_form', views.process)
+    path('process', views.process),
+    path('results', views.results)
 ]
--- django_files/forms/form_proj/form_app/views.py
+++ django_files/forms/form_proj/form_app/views.py
@@ -2,10 +2,22 @@ from django.shortcuts import render, redirect, HttpResponse
 
 # Create your views here.
 def index(request):
+    ##print(request.POST['username']) You cannot access information from another route
     return render(request,'index.html')
 
 def process(request):
     if request.method != 'POST':
         return redirect('/')
-    print(request.POST)
-    return HttpResponse('This is a post route')
+    print(request.POST['username'])
+    print(request.POST['email'])
+    print(request.POST['password'])
+    return redirect("/results")
+## never render on a post
+
+def results(request):
+    context = {
+    'username': request.POST['username'],
+    'email': request.POST['email'],
+    'password': request.POST['password']
+    }
+    return render(request, 'results.html', context)

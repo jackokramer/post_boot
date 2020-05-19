cf2ea137c9455b340facfc90db3bca8528b514ec
jackokramer <jackokramer@gmail.com>
2020-05-19 12:29:42 -0700

    hail satan

--- python_excer/OOP/binarysearchtree.js
+++ python_excer/OOP/binarysearchtree.js
@@ -7,5 +7,22 @@ class BSTNode{
 }
 
 class BinarySearchTree{
-    
+    constructor(){
+        this.root=null;
+    }
+    addNode(val, position = this.root){
+        if(this.root == null){
+            this.root = new BSTNode(val);
+            return;
+        }
+        else if(val <position.value){
+            if(position.left == null){
+                position.left = new BSTNode(val);
+                return;
+            }
+            else{
+                return this.addNode(val, position.left)
+            }
+        }
+    }
 }

f38f18dd9ecf6300f49e5650f6fe4f8a02724b72
jackokramer <jackokramer@gmail.com>
2020-05-21 13:09:54 -0700

    runner

---
+++ django/djang_api/stash/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/jackquentinkramer/Desktop/post_doj/django/djang_api/stash/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()

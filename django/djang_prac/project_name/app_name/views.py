0a15c2a83cd7e1c35fe25a0dce899eae69ce247e
jackokramer <jackokramer@gmail.com>
2020-05-25 20:23:38 -0700

    rain

--- django/djang_prac/project_name/app_name/views.py
+++ django/djang_prac/project_name/app_name/views.py
@@ -1,13 +1,26 @@
 from django.shortcuts import render, HttpResponse, redirect
+import random
 
 # Create your views here.
 def index(request):
+    if 'results' not in request.session:
+        request.session['results'] = []
     return render(request, 'index.html')
 
 def process(request):
     print(request.method)
     if request.method == "POST":
-        ## play the game
+        com_num = int(random.random()*10)
+        if int(request.POST['user_guess'])>com_num:
+            request.session['results'].append(f"{request.POST['user_name']} guessed too high. Guess: {request.POST['user_guess']} was higher than {com_num}")
+        elif int(request.POST['user_guess'])<com_num:
+            request.session['results'].append(f"{request.POST['user_name']} guessed too low. Guess: {request.POST['user_guess']} was less than {com_num}")
+        else:
+            request.session['results'].append(f"{request.POST['user_name']}) You guessed the number{com_num}"
+        #print(com_num, 'this is the random number generated')
+        print(request.POST, "this is my request.post")
+        print(request.POST['user_name'])
+        print(request.POST['user_guess'])
         return redirect("/")
     #request.POST['username']
     return redirect("/")

bffdb010d2c62f8ba23ea325c76becd833234bf8
jackokramer <jackokramer@gmail.com>
2020-05-31 13:35:50 -0700

    sare

--- django/dojo_survey/survey_proj/survey_app/views.py
+++ django/dojo_survey/survey_proj/survey_app/views.py
@@ -3,3 +3,8 @@ from django.shortcuts import render, HttpResponse, redirect
 # Create your views here.
 def index(request):
     return render(request, "index.html")
+
+def results(request):
+    # if request.method == "POST"
+    print(request.POST)
+    return render(request, "results.html")

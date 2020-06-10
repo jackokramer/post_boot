f36867f75452a841274a24e0475255ebdb4718fb
jackokramer <jackokramer@gmail.com>
2020-06-09 20:40:40 -0700

    arew

--- django/djang_orm/survey/survey/app_level/models.py
+++ django/djang_orm/survey/survey/app_level/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+class Users(models.Model):
+    first_name = models.CharField(max_length=45)
+    last_name = models.CharField(max_length=45)
+    email = models.CharField(max_length=45)
+    location = models.CharField(max_length=45)
+    language = models.CharField(max_length=45)
+    comment = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
 # Create your models here.

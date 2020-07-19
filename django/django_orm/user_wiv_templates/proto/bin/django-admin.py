1a6fe4623bde93bbafa91d85dfeffe0eb1d03e8e
jackokramer <jackokramer@gmail.com>
2020-07-19 10:37:22 -0700

    suns

---
+++ django/django_orm/user_wiv_templates/proto/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/jackquentinkramer/Desktop/post_doj/django/django_orm/user_wiv_templates/proto/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()

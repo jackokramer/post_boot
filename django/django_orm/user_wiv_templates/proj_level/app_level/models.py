c72fe404d98d194a863de3095170c5d88583dda6
jackokramer <jackokramer@gmail.com>
2020-07-19 20:41:32 -0700

    sundays

--- django/django_orm/user_wiv_templates/proj_level/app_level/models.py
+++ django/django_orm/user_wiv_templates/proj_level/app_level/models.py
@@ -1,5 +1,10 @@
 from django.db import models
 
 # Create your models here.
-def index(request):
-    return render(request, "index.html")
+class Users(models.Model):
+    first_name = models.CharField(max_length=45)
+    last_name = models.CharField(max_length=45)
+    email = models.CharField(max_length=45)
+    age = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
--- django/django_orm/user_wiv_templates/proj_level/app_level/views.py
+++ django/django_orm/user_wiv_templates/proj_level/app_level/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Users
 
 # Create your views here.
+def index(request):
+    context = {
+        'users': Users.objects.all()
+    }
+    return render(request, "index.html", context)
+
+def finished(request):
+    Users.objects.create(
+        first_name = request.POST['first_name'],
+        last_name = request.POST['last_name'],
+        email = request.POST['email'],
+        age = reques.POST['age'],
+    )
+    return render("/")

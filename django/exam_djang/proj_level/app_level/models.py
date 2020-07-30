147e1b858d0f325086879a4c74ccf3abcb8a5177
jackokramer <jackokramer@gmail.com>
2020-07-30 12:51:54 -0700

    completed a survey form in djang_exam showcasing all of mbappe`s fans

--- django/exam_djang/proj_level/app_level/models.py
+++ django/exam_djang/proj_level/app_level/models.py
@@ -5,6 +5,7 @@ class User(models.Model):
     first_name = models.CharField(max_length=45)
     last_name = models.CharField(max_length=45)
     email = models.CharField(max_length=200)
+    ##language = models.CharField(max_length=200)
     age = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)

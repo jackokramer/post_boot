951ae1851e7cb7e563e5a9a12b0802b5e91a72db
jackokramer <jackokramer@gmail.com>
2020-07-30 12:11:20 -0700

    practicing my orm queries with django under exam_djang

---
+++ django/exam_djang/earner/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/jackquentinkramer/Desktop/post_doj/django/exam_djang/earner/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()

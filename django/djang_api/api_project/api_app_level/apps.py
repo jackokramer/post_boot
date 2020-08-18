33e11dc505298245837762d4e6887bb0e37d9c4b
jackokramer <jackokramer@gmail.com>
2020-08-17 23:51:20 -0700

    semi_rest

--- django/djang_api/api_project/api_app_level/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ApiAppLevelConfig(AppConfig):
-    name = 'api_app_level'
--- django/exam_review/proj_level/app_level/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index),
-    path('process', views.process),
-    path('login', views.login),
-    path('success', views.success)
-]

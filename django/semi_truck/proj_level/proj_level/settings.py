d9f15c60fde795579d6845cd4885766100411bef
jackokramer <jackokramer@gmail.com>
2020-08-19 17:51:58 -0700

    semi rest almost done just the update isnt working

--- django/semi_truck/proj_level/proj_level/settings.py
+++ django/semi_truck/proj_level/proj_level/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '^#afn6*j=pih34lm1$hv-rwwpucu)=ppuw2up^c5f&fkjt5e1y'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

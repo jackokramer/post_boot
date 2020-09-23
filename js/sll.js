21ed1ea204d2af8a6f684789aec03c3b60c6bb4e
jackokramer <jackokramer@gmail.com>
2020-09-23 11:04:43 -0700

    sll practice

--- js/sll.js
+++ js/sll.js
@@ -1,3 +1,5 @@
+//sll are effeicnietn because the place in storage is at the previous node
+
 class Node{
     constructor(value){
         this.value = value;
@@ -17,24 +19,47 @@ class SLL{
     constructor(value){
         this.head = new Node(value);
     }
+    //add front adding something into the list needs a value. PLace in memeory is the pointer of the previous node
     addFront(value){
         let restOfList = this.head;
         this.head = new Node(value);
         this.head.next = restOfList;
         return this;
     }
+    //front - to get data
+    front(){
+        if(this.head !== null){
+            return this.head.value
+        } else {
+            return 'this is an empty list'
+        }
+    }
+    // remove front no need for a parameter since we aren't adding anything
+    removeFront(){
+        this.head = this.head.next;
+        return this;
+    }
 
+    //contains: given a value, return true if the value is in your list. else return false
+    contains(value){
+        if(val )
+
+    }
+    //display
     displayList(){
         let runner = this.head;
         let count = 1;
         while(runner){
-            console.log(`Current node: ${runner}, current value: ${runner.value}, COunt: ${count}`)
+            console.log(`Current node: ${runner}, current value: ${runner.value}, Count: ${count}`)
             runner = runner.next;
             count +=1;
         }
-        return this
-    }
+        return "*** TEST CONCLUDED ***";
+        }
 }
 
 let firstSll = new SLL(2);
-console.log(firstSll.addFront(3).addFront(6).addFront(8).addFront(2).displayList())
+console.log(firstSll.addFront(3).addFront(6).addFront(8).addFront(17).displayList())
+//console.log(firstSll.removeFront().displayList())
+
+console.log(firstSll.front())

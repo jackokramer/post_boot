2a74c7132de4585ddb5cc0f2c89d3e3ef023014c
jackokramer <jackokramer@gmail.com>
2020-09-23 11:24:51 -0700

    sll practice

--- js/sll.js
+++ js/sll.js
@@ -42,20 +42,73 @@ class SLL{
 
     //contains: given a value, return true if the value is in your list. else return false
     contains(value){
-        if(val )
-
+            let runner = this.head;
+            while(runner){
+                if(runner.value === value){
+                    return true
+                }
+                runner = runner.next;
+            }
+            return false
+    }
+    //length: return # of nodes in the list
+    length(){
+        return this.displayList()
+    }
+    //max
+    max(){
+        let max = this.head.value;
+        let runner = this.head;
+        while(runner){
+            if(max<runner.value){
+                max = runner.value;
+            }
+            runner = runner.next;
+        }
+        return max;
+    }
+    // min
+    min(){
+        let min = this.head.value;
+        let runner = this.head;
+        while(runner){
+            if(min>runner.value){
+                min = runner.value;
+            }
+            runner = runner.next;
+        }
+        return min;
+    }
+    //average
+    average(){
+        let sum = 0;
+        let runner = this.head;// any time you'd traverse through a linked lis create a runner
+        while(runner){
+            sum += runner.value
+            runner=runner.next
+        }
+        return sum/this.length()
     }
     //display
     displayList(){
         let runner = this.head;
         let count = 1;
         while(runner){
-            console.log(`Current node: ${runner}, current value: ${runner.value}, Count: ${count}`)
+            //console.log(`Current node: ${runner}, current value: ${runner.value}, Count: ${count}`)
             runner = runner.next;
             count +=1;
         }
-        return "*** TEST CONCLUDED ***";
+        return count //"*** TEST CONCLUDED ***";
         }
+    //utlimate analysis return a dictionary containing max, min, avg, and length
+    ultimareAnalysis(){
+        return{
+            'max': this.max(),
+            'min': this.min(),
+            'average': this.average(),
+            'length': this.length()
+        }
+    }
 }
 
 let firstSll = new SLL(2);
@@ -63,3 +116,5 @@ console.log(firstSll.addFront(3).addFront(6).addFront(8).addFront(17).displayLis
 //console.log(firstSll.removeFront().displayList())
 
 console.log(firstSll.front())
+console.log(firstSll.contains(7))
+console.log(firstSll.ultimareAnalysis())

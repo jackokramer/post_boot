d6176df97a867a88bf434dd20bd89d08d3a30c84
jackokramer <jackokramer@gmail.com>
2020-09-30 10:05:32 -0700

    data structure complete now onto site functionality

--- django/ecommerce/ecomm_proj/store/models.py
+++ django/ecommerce/ecomm_proj/store/models.py
@@ -15,10 +15,19 @@ class Product(models.Model):
     name = models.CharField(max_length=100, null=True)
     price = models.FloatField()
     digital = models.BooleanField(default=False, null=True, blank=True) ## know if the item is digital and  does not need to be shipped
+    image = models.ImageField(null=True, blank=False)
     #image
 
     def __str__(self):
         return str(self.name)
+
+    @property
+    def imageURL(self):
+        try:
+            url = self.image.url
+        except:
+            url=''
+        return url
 
 class Order(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, blank=True)
@@ -29,11 +38,28 @@ class Order(models.Model):
     def __str__(self):
         return str(self.id)
 
+    @property
+    def get_cart_total(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.get_total for item in orderitems]) #total price in the cart
+        return total
+
+    @property
+    def get_cart_items(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.quantity for item in orderitems]) #how many items are in your cart
+        return total
+
 class OrderItem(models.Model):
     product = models.ForeignKey(Product, on_delete=models.SET_NULL, blank=True, null=True)
     order = models.ForeignKey(Order, on_delete=models.SET_NULL, blank=True, null=True)
     quantity = models.IntegerField(default=0, null=True, blank=True)
     date_added = models.DateTimeField(auto_now_add=True)
+
+    @property ## total value of the orderItem
+    def get_total(self):
+        total = self.product.price * self.quantity
+        return total
 
 class ShippingAddress(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, blank=True, null=True)
--- django/ecommerce/ecomm_proj/store/views.py
+++ django/ecommerce/ecomm_proj/store/views.py
@@ -8,9 +8,25 @@ def store(request):
     return render(request, 'store/store.html', context)
 
 def cart(request):
-    context = {}
+    if request.user.is_authenticated: #two condtions one authenticated
+        customer = request.user.customer #one to one relation that way
+        order, created = Order.objects.get_or_create(customer=customer, complete=False) ##looks for an existing user if the user does not exist then it creates them
+        items = order.orderitem_set.all() #query child ojects with the parent value to the childvalye and set them to smaller case
+    else:
+        items = []
+        order = {'get_cart_total': 0, 'get_cart_items': 0 } ##needs to be added to create an unathenticated user
+
+    context = {'items': items, 'order': order}
     return render(request, 'store/cart.html', context)
 
 def checkout(request):
-    context = {}
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        ## create an empty cart
+        order= {'get_cart_total': 0, 'get_cart_items':0}
+        items = []
+    context = {'items':items, 'order': order}
     return render(request, 'store/checkout.html', context)

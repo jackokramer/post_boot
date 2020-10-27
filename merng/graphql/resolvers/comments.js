7ef3cfc53feb381b48185ea050d6ae9a5997abe9
jackokramer <jackokramer@gmail.com>
2020-10-27 09:54:06 -0700

    push merng

--- merng/graphql/resolvers/comments.js
+++ merng/graphql/resolvers/comments.js
@@ -1,4 +1,4 @@
-const {UserInputError} = require('apollo-server')
+const {AuthenticationError, UserInputError} = require('apollo-server')
 
 const checkAuth = require('../../util/check-auth')
 const Post = require('../../models/Post');
@@ -28,6 +28,23 @@ module.exports = {
                 throw new UserInputError('Post not found')
             }
         },
-        async deleteComment
+        async deleteComment(_, {postId, commentId}, context){
+            const {username} = checkAuth(context);
+
+            const post = await Post.findById(postId);
+
+            if(post){
+                const commentIndex = post.comments.findIndex(c => c.id === commentId)
+                if(post.comments[commentIndex].username === username){
+                    post.comments.splice(commentIndex, 1);
+                    post.save();
+                    return post;
+                } else {
+                    throw new AuthenticationError('Action not allowed')
+                }
+            } else {
+                throw new UserInputError('user not found')
+            }
+        }
     }
 }

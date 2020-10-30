5fef253f8190405328441a653a7e9cad3497a7b9
jackokramer <jackokramer@gmail.com>
2020-10-30 09:20:41 -0700

    push

--- js/json_refresh/get.js
+++ js/json_refresh/get.js
@@ -1,7 +1,14 @@
         $(document).ready(function(){
             $.get("https://pokeapi.co/api/v2/pokemon/1/", function(res){
-                console.log(res.types[0].type.name);
-                console.log(res.types[1].type.name);
-                $('#more-info').html("<p>" + res.abilities[0].name +"</p>")
+//                console.log(res.types[0].type.name);
+//                console.log(res.types[1].type.name);
+                let html_str = "";
+                html_str = "<h4>Types</h4>";
+                html_str = "<ul>";
+                for(let x =0; x<res.types.length; x++){
+                    html_str +="<li>"+res.types[x].type.name+"</li>"
+                }
+                html_str = "</ul>";
+                $('#more-info').html(html_str)
             }, 'json')
         })

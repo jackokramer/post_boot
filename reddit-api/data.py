28cc35e3b8d5266c53e7fbba6907a4b7aed8afc4
jackokramer <jackokramer@gmail.com>
2020-11-07 20:20:48 -0800

    more apis

--- reddit-api/data.py
+++ reddit-api/data.py
@@ -16,3 +16,26 @@ data_JSON = ""
 data_dict = json.loads(data_JSON)
 
 print(data_dict)
+
+{
+    "orders":[
+    {
+        "size":"medium",
+        "price": 17.90,
+        "toppings": [
+            "mushrooms",
+            "pepercini",
+            "basil"
+        ],
+        "extra cheese": false,
+        "delivery": true
+    },
+    {
+        "size": "small",
+        "price": 6.49,
+        "toppings": null,
+        "extra_cheese": true,
+        "delivery": false,
+    }
+    ]
+    }

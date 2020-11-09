54f8478b552c8be3220d3b561f65dde0361d36ca
jackokramer <jackokramer@gmail.com>
2020-11-08 19:03:22 -0800

    more reddit apis

--- reddit-api/data.py
+++ reddit-api/data.py
@@ -39,3 +39,14 @@ print(data_dict)
     }
     ]
     }
+
+    ## serialization converting an object to a string
+
+    ## deserialization converting a json string into an object
+
+
+    ## JSON (javascript) object notation is a format used to represent and store data.
+    ## commonly used to transfer data on the web and to store configuration settings
+    ## you can convert json strings into python objects and vice versa.
+    ## you can read json files and create python objects from their key value pairs.
+    ## You can write to JSON files  to store the content of python objects in json format.

487520f8f513c9e981cddeef89e06bcb8d92bee4
jackokramer <jackokramer@gmail.com>
2020-11-14 23:04:42 -0800

    pushed new stuff

--- react/tuts2/src/index.js
+++ react/tuts2/src/index.js
@@ -6,27 +6,46 @@ import ReactDom from 'react-dom';
 
 //CSS
 import './index.css'
-
-const author = "Ann Whitford Paul";
-const title = 'I Animals Kissed Good Night';
-const image =  'https://images-na.ssl-images-amazon.com/images/I/817T4J3dzhL._AC_UL200_SR200,200_.jpg'
+const books = [
+  {
+  id:1,
+  img:"https://images-na.ssl-images-amazon.com/images/I/81hx39H8JEL._AC_UL200_SR200,200_.jpg",
+  title: "The World Needs Who You Were Made to Be",
+  author: "Joanna Gaines"
+},
+{
+  id:2,
+  img:"https://images-na.ssl-images-amazon.com/images/I/817T4J3dzhL._AC_UL200_SR200,200_.jpg",
+  title: "If Animals Kissed Good Night",
+  author: "Ann Whitford Paul"
+},
+{
+  id:3,
+  img:"https://images-na.ssl-images-amazon.com/images/I/81GeAcdMCsL._AC_UL200_SR200,200_.jpg",
+  title: "Humans",
+  author: "Brandon Stanton"
+}
+]
 
 function BookList(){
   return (
     <section className="booklist">
-  <Book/>
+      {books.map((book, index)=> {
+        return <Book key={book.id} {...book}></Book>
+      })}
     </section>
   )
 }
 const Book = (props) =>{
+  console.log(props)
+  const{img, title, author} = props;
   return ( <article className="book">
-    <img src={image}  alt=""/>
+    <img src={img}  alt=""/>
 <h1>{title}</h1>
 <h4>{author}</h4>
-<p>{6+6+6}</p>
     </article>
   )
 }
-
+//using props allows you to set up unique values
 
 ReactDom.render(<BookList/>, document.getElementById('root'))

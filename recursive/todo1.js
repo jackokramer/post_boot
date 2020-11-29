d88f9be062262dce3049bb2add6cdf2039fad6f9
jackokramer <jackokramer@gmail.com>
2020-11-28 23:18:44 -0800

    recustion

--- recursive/todo1.js
+++ recursive/todo1.js
@@ -91,7 +91,20 @@ rBinarySearch([1,3,5,6],4) = false;
 
 rBinarySearch([4,5,6,8,12],5) = true.
 */
-function rBinarySearch(nums, number){
+function rBinarySearch(nums, value){
+    let mid = Math.floor(nums.length/2);
+    if(nums[mid] == value){
+        return true;
+    }
+    else if(value < nums[mid] && nums.length > 1) {
+        return rBinarySearch(arr.slice(0, mid), value);
+    }
+    else if(value>nums[mid] && nums.length>1){
+        return rBinarySearch(nums.slice(mid, arr.length), value)
+    } else {
+        return false
+    }
+    /*
     if(nums === number){
         return true;
     } else if(nums != number) {
@@ -100,6 +113,39 @@ function rBinarySearch(nums, number){
         } else if(nums<number){
             nums + rBinarySearch/2
         }
-    }
-    return nums
+    } */
 }
+
+console.log(rBinarySearch([1,3,4,5,9,11],7))
+
+/*
+Greatest Common Factor
+Given two integers, create rGCF(num1,num2) to recursively determine Greatest Common Factor (the largest integer dividing evenly into both). Greek mathematician Euclid demonstrated these facts:
+
+gcf(a,b) == a, if a == b;
+gcf(a,b) == gcf(a-b,b), if a>b;
+gcf(a,b) == gcf(a,b-a), if b>a.
+Second: rework facts #2 and #3 to reduce stack consumption and expand rGCF’s reach. You should  be able to compute rGCF(123456,987654).
+*/
+
+function rGCG(num1, num2){
+    if(num1 == num2){
+        return num1;
+    }
+    if(num1 >num2){
+        return rGCG(num1-num2, num2)
+    }
+    if(num1<num2){
+        return rGCG(num1, num2-num1);
+    }
+}
+
+console.log(rGCG(3,9))
+///
+/*Tarai
+The Tarai (Japanese: “to pass around”) function was created to profile recursive performance in various systems and languages. Unlike other functions, numbers don't get particularly large, but the amount of recursion is significant. The tarai function accepts three parameters and is defined as:
+
+tarai(x,y,z) == y, if x <= y (otherwise see rule #2);
+tarai(x,y,z) == tarai(tarai(x-1,y,z),tarai(y-1,z,x),tarai(z-1,x,y)).
+Calling tarai(10,2,9) should return 9 (after recursing 4145 times!).
+*/

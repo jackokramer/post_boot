8047f42c7ea3ea6f8c35cac4e648016fd518a186
jackokramer <jackokramer@gmail.com>
2020-12-03 21:29:11 -0800

    more angular

--- angular/app/sample/src/app/app.component.ts
+++ angular/app/sample/src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { templateJitUrl } from '@angular/compiler';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +8,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  user = {first_name: '', last_name:""};
   title = 'sample size';
   myObject = {name: 'Tina', status: "lard ass"}
+  someEvent(num){
+    console.log("AppComponent > someEvent", num)
+  }
+  mouseOver(){
+    console.log('log the data')
+  }
+  onSubmit(){
+    console.log(this.user)
+  }
 }
+
+@FormsModule({
+    selector: 'app-root',
+    templateUrl: './app.component.html'
+})

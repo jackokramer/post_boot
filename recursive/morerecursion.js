9ba7f61af448da0749343ed754cc37e90d19f482
jackokramer <jackokramer@gmail.com>
2020-12-07 16:52:05 -0800

    practice

--- recursive/morerecursion.js
+++ recursive/morerecursion.js
@@ -1,10 +1,12 @@
 // WRITING VALUES INTO ARRAYS
 
-let arr = [2,4,6,8] // create an array with four distinct values
+let arr = [2, 4, 6, 8] // create an array with four distinct values
 
 
-arr[1]= 10 
+arr[1] = 10
 
 //return arr
 
 console.log(arr);
+
+/////

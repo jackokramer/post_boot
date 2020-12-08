b65aade245fa83856bc0f3bd3234399db808cd99
jackokramer <jackokramer@gmail.com>
2020-12-08 14:30:19 -0800

    more recursion

--- recursive/morerecursion.js
+++ recursive/morerecursion.js
@@ -10,3 +10,4 @@ arr[1] = 10
 console.log(arr);
 
 /////
+
--- recursive/todo1.js
+++ recursive/todo1.js
@@ -149,3 +149,10 @@ tarai(x,y,z) == y, if x <= y (otherwise see rule #2);
 tarai(x,y,z) == tarai(tarai(x-1,y,z),tarai(y-1,z,x),tarai(z-1,x,y)).
 Calling tarai(10,2,9) should return 9 (after recursing 4145 times!).
 */
+
+/**Recursive List Length
+Given the first node of a singly linked list, create a recursive function that returns the number of nodes in that list. Assume list contains no loops and is short enough that you will not ‘blow your stack’. */
+
+function recursiveListLength(){
+    
+}
